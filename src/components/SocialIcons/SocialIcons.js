75b241806b0d9536306d9287b39f8ee9ccf1f003
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2018-10-12 11:15:36 -0400

    updated link

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -37,7 +37,7 @@ const SocialIcons = (props, context) => {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="/resume.pdf"
+        href="%PUBLIC_URL%/resume.pdf"
         style={{ color: colorPrimary }}
       >
         <i className="far fa-file-pdf" />

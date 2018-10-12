17ae80074ce8d4322de95b86e35f23a226fb89a6
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2018-10-12 11:10:59 -0400

    updated resume link

--- src/components/SocialIcons/SocialIcons.js
+++ src/components/SocialIcons/SocialIcons.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './style.scss';
+import "./style.scss";
 
 const SocialIcons = (props, context) => {
-  const { theme: { colorPrimary } } = context;
-  
+  const {
+    theme: { colorPrimary }
+  } = context;
+
   return (
     <div className="social-icons animate-icons">
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/skananit" style={ { color: colorPrimary } }><i className="fab fa-github"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/shimakanani/" style={ { color: colorPrimary } }><i className="fab fa-linkedin"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@shima.kanani.t" style={ { color: colorPrimary } }><i className="fab fa-medium"></i></a>
-        <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1DYFKC6SEr9bQeN9FxZoSqi39Je7DR9Ev?usp=sharing" style={ { color: colorPrimary } }><i className="far fa-file-pdf"></i></a>
-
-      </div>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/skananit"
+        style={{ color: colorPrimary }}
+      >
+        <i className="fab fa-github" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/shimakanani/"
+        style={{ color: colorPrimary }}
+      >
+        <i className="fab fa-linkedin" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://medium.com/@shima.kanani.t"
+        style={{ color: colorPrimary }}
+      >
+        <i className="fab fa-medium" />
+      </a>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="/resume.pdf"
+        style={{ color: colorPrimary }}
+      >
+        <i className="far fa-file-pdf" />
+      </a>
+    </div>
   );
 };
 

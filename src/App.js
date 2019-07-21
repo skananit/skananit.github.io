006099b3009a3c909aa29d2f6fea5d614f3815af
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2019-07-20 22:06:28 -0400

    contact page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import "./App.css";
 import TimelinePage from "./components/TimelinePage/TimelinePage.js";
 import LandingPage from "./components/LandingPage/LandingPage.js";
 import PortfolioPage from "./components/PortfolioPage/PortfolioPage.js";
-import AboutPage from "./components/AboutPage/AboutPage";
 import ContactPage from "./components/ContactPage/ContactPage.js";
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
-      {/* <AboutPage /> */}
       <PortfolioPage />
       <TimelinePage />
       <ContactPage />

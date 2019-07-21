5f743a500111d5f50b92894b9bb4c5a94c6d617c
shimakanani <Shima@Shimas-MacBook-Pro-3.local>
2019-07-20 22:35:01 -0400

    new-look

---
+++ precache-manifest.c2020d6ae5a6ff8c7bffb2538d786e40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d133d321570400ce2cdc96152df22b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c9c4eea81dc806d8349",
+    "url": "/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "57aee032044c148845d7",
+    "url": "/static/css/main.33f3e8c1.chunk.css"
+  },
+  {
+    "revision": "2c9c4eea81dc806d8349",
+    "url": "/static/js/2.9a45faad.chunk.js"
+  },
+  {
+    "revision": "57aee032044c148845d7",
+    "url": "/static/js/main.443b7aad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

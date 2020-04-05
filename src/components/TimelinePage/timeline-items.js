67b4d3fa7ea621f0adfbae4dc9b845b4df0ca22a
SHIMA KANANITODASHKI <skananit@uwo.ca>
2020-04-04 21:47:06 -0400

    updated info

--- src/components/TimelinePage/timeline-items.js
+++ src/components/TimelinePage/timeline-items.js
@@ -13,9 +13,9 @@ export default [
       "Joining Amazon's Toronto office as a fulltime Software Engineer in June 2020.",
     category: {
       tag: "work",
-      color: workColor
+      color: workColor,
     },
-    href: "https://www.scotiabank.com/gls/en/index.html#about"
+    href: "https://www.scotiabank.com/gls/en/index.html#about",
   },
   {
     role: "Product Manager Intern",
@@ -27,9 +27,9 @@ export default [
       "I trained as a product manager on two Capital Markets Analytics products at the digital banking unit in Waterloo and successfully delivered the initial phase of the project.  The project was the highest priority Artificial Intelligence project in Scotiabank’s Global Banking and Markets;  expected to generate $9-12M in revenues annually. As the Product Manager, I synthesized stakeholders’ expectations, including managing directors, executives, and end-users in Debt and Equity Capital Markets to prioritize product features and create a concrete product vision.",
     category: {
       tag: "work",
-      color: workColor
+      color: workColor,
     },
-    href: "https://www.scotiabank.com/gls/en/index.html#about"
+    href: "https://www.scotiabank.com/gls/en/index.html#about",
   },
   {
     role: "Software Engineer Intern",
@@ -41,9 +41,9 @@ export default [
       "Along with my product management training, I was a core software engineer on the engineering team that delivered the frontend application. I developed the frontend components of the predictive analytics tool using AngularJS; components included financial charts such as yield curves and debt breakdowns, instrument tables, and functionalities for filtering, search, and navigation. ",
     category: {
       tag: "work",
-      color: workColor
+      color: workColor,
     },
-    href: "https://www.scotiabank.com/gls/en/index.html#about"
+    href: "https://www.scotiabank.com/gls/en/index.html#about",
   },
   // {
   //   role: "Software Engineer",
@@ -69,9 +69,9 @@ export default [
       "SheHacks is Canada’s largest 12-hour all-women's hackathon. I helped hackers brainstorm ideas on the challenge, work through the solutions, and gave advice on software development and presentation.  ",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "http://shehacks.ca"
+    href: "http://shehacks.ca",
   },
   {
     role: "Campus Ambassador",
@@ -83,9 +83,9 @@ export default [
       "Communitech is Canada's biggest community of tech startups with a goal           of establishing the Waterloo region as a hot spot for technological           innovation by inspiring and retaining new talent in the area. As a           Campus Ambassador, I encourage entrepreneurship and innovation in tech           by organizing tech-related events on campus and promoting Waterloo's           tech ecosystem through media marketing, hackathons, and job fairs.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "http://communitech.ca"
+    href: "http://communitech.ca",
   },
   {
     role: "Bachelor's Degree, Honors Business Administration ",
@@ -97,9 +97,9 @@ export default [
       " Ivey is famous for its practical, collaborative teaching approach by           relying extensively on the case method of study. Learning is done           through working with actual business challenges faced by real           managers. Every day in class we analyze information, make decisions,           and defend those decisions when others around us may disagree. As a           dual degree student, I take Software Engineering and Business courses           over the span of five years at Western's Engineering Faculty and Ivey           Business School.",
     category: {
       tag: "education",
-      color: educationColor
+      color: educationColor,
     },
-    href: "http://ivey.uwo.ca"
+    href: "http://ivey.uwo.ca",
   },
   {
     role: "Software Engineer Intern",
@@ -111,9 +111,9 @@ export default [
       "Scotiabank’s Digital Banking Unit in Waterloo consisted of a small,           yet tight-knit team of user experience designers and engineers. I           completed a 16-week internship and delivered a classified project           aiming to transform the future of banking by redefining the           relationship a customer has with their finances. My role involved           developing an app (full stack) in an agile team of engineers with a           test-driven, code review culture. Plus, I had the chance to be a part           of every step; from idea generation to the final delivery. Therefore,           I had to think strategically and go beyond my role in order to           implement the project stakeholders' feedbacks into our project. As the           project was completed end-to-end, our app will soon be available on           Google Play and App Store given that it is a seasonal product.",
     category: {
       tag: "work",
-      color: workColor
+      color: workColor,
     },
-    href: "https://www.scotiabank.com/gls/en/index.html#about"
+    href: "https://www.scotiabank.com/gls/en/index.html#about",
   },
 
   {
@@ -126,9 +126,9 @@ export default [
       "International Association for the Exchange of Students for Technical           Experience (IAESTE) is a unique organization that provides students           with the valuable experience of working abroad. As president of the           local chapter in London, I lead a team of 14 executives by           establishing common goals, supervising assigned projects, and creating           a plan to meet the national goals of the organization. Activities of           our local chapter include planning, promoting, and facilitating           international internships.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "https://iaeste.org"
+    href: "https://iaeste.org",
   },
   {
     role: "Program Manager",
@@ -140,9 +140,9 @@ export default [
       " W5 is an idea accelerator designed to help idea stage entrepreneurs           bring their idea to market in five months. As the program director, I           partnered with successful entrepreneurs, industry experts, and partner           companies to help our cohort of 15 startups achieve their objectives,           and turn their idea into a business. W5 startups advanced from the           ideation stage to growth and amassed over 10 million app downloads           with 1.5 million in revenue.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "https://www.w5entrepreneurs.com"
+    href: "https://www.w5entrepreneurs.com",
   },
   {
     role: "Recruitment Manager",
@@ -154,9 +154,9 @@ export default [
       "International Association for the Exchange of Students for Technical Experience (IAESTE) is a unique organization that provides students with the valuable experience of working abroad. Eager to provide our campus with such opportunities, I got involved with London's local chapter in 2017. As Recruitment Manager, I recruited seven local companies and five university professors to participate in our exchange program.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "https://iaeste.org"
+    href: "https://iaeste.org",
   },
   {
     role: "Technology Team Lead",
@@ -168,9 +168,9 @@ export default [
       "MEDLIFE improves the access to MEDs: Medicine, Education, and Development in poor communities by freeing families from the constraints of poverty, empowering them to live healthier lives. As the lead of the technology committee within the local chapter, I built MEDLIFE's online presence in London. I expanded the chapter's reach by opening a membership portal on the website and contributed to the cause of the organization through the online distribution and promotion of medical education and development.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "https://www.medlifemovement.org"
+    href: "https://www.medlifemovement.org",
   },
 
   {
@@ -183,9 +183,9 @@ export default [
       "WEC is Western's biggest engineering competition taking place annually. The winners from this competition continue to compete at the Ontario Engineering Competition (OEC). As the recruitment lead, I recruited diverse judging panels consisting of industry professionals, professors, and alumni for all six categories and acted as the main point of contact between the event organizers and the judges throughout the recruitment process as well as the competition.",
     category: {
       tag: "extra",
-      color: extraColor
+      color: extraColor,
     },
-    href: "https://www.eng.uwo.ca/"
+    href: "https://www.eng.uwo.ca/",
   },
   // {
   //   role: "Co-founder & President",
@@ -201,20 +201,20 @@ export default [
   //   },
   //   href: "https://www.girlplusplus.ca"
   // },
-  {
-    role: "Technical Analyst",
-    company: "World of Forex",
-    logo: "/images/wof.png",
-    location: "Toronto, ON",
-    date: "Summer 2016 ",
-    description:
-      "As a technical Analyst at World of Forex, I provided technical support for the company's software used to trade on the Foreign Exchange market. I studied end-user problems and network performance to find the root cause of incidents and researched the required technical actions to improve the user experience. By analyzing user behavior data, I was able to make recommendations that increased user satisfaction by 35%.",
-    category: {
-      tag: "work",
-      color: workColor
-    },
-    href: "http://worldofforex.com"
-  },
+  // {
+  //   role: "Technical Analyst",
+  //   company: "World of Forex",
+  //   logo: "/images/wof.png",
+  //   location: "Toronto, ON",
+  //   date: "Summer 2016 ",
+  //   description:
+  //     "As a technical Analyst at World of Forex, I provided technical support for the company's software used to trade on the Foreign Exchange market. I studied end-user problems and network performance to find the root cause of incidents and researched the required technical actions to improve the user experience. By analyzing user behavior data, I was able to make recommendations that increased user satisfaction by 35%.",
+  //   category: {
+  //     tag: "work",
+  //     color: workColor
+  //   },
+  //   href: "http://worldofforex.com"
+  // },
   {
     role: "Bachelor's Degree, Software Engineering ",
     company: "World of Forex",
@@ -225,8 +225,8 @@ export default [
       "Entered Western University as an Engineering student awarded the Western Excellence scholarship.",
     category: {
       tag: "education",
-      color: educationColor
+      color: educationColor,
     },
-    href: "http://worldofforex.com"
-  }
+    href: "http://worldofforex.com",
+  },
 ];

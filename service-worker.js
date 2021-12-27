a07ad91058d9c3c6ab18a8420a3cdb284068e5dd
SHIMA KANANITODASHKI <skananit@uwo.ca>
2021-12-27 13:38:27 -0500

    Updated website content.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be7598bf5f8b51795dfa00d38b8c38fc.js"
+  "/precache-manifest.18a75c887c277de77597c1665ab9bbd9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

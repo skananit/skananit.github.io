7362875fcd6c3e925e8f718afeda2916ca1f8b91
SHIMA KANANITODASHKI <skananit@uwo.ca>
2022-01-05 12:00:01 -0500

    Updated content.

---
+++ precache-manifest.0f9d0c9fd19e662f53ab800ce4f06f62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae0754ee4f3e4ca9ce1a78de1f9e734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "738c446f1dbbb76138d0",
+    "url": "/static/css/main.33ce6fb9.chunk.css"
+  },
+  {
+    "revision": "5566fa1fb0f80fc60fe5",
+    "url": "/static/js/2.28a3e218.chunk.js"
+  },
+  {
+    "revision": "c958d05fd858cb9be7095762ec45228f",
+    "url": "/static/js/2.28a3e218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738c446f1dbbb76138d0",
+    "url": "/static/js/main.d98a9051.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
